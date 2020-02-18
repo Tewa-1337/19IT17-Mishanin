14301325a9825e3eea177e7c60ffcb1fe2d4bfdd
Tewa-1337 <60029473+Tewa-1337@users.noreply.github.com>
2020-02-18 09:38:05 +0300

    Add files via upload

---
+++ Частотный анализ.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            foreach (int x in arr)
+            {
+                arr[x] = 0;
+            }
+            int checkout = 0;//рандомятся числа до того момента, пока не введем -1
+            while (checkout != -1)
+            {
+                int k = rnd.Next(1, n);
+                arr[k]++;
+                checkout = int.Parse(Console.ReadLine());
+            }
+            //сортировка 
+            int temp;
+            for (int r = 0; r < arr.Length; r++)
+            {
+                for (int o = r + 1; o < arr.Length; o++)
+                {
+                    if (arr[r] > arr[o])
+                    {
+                        temp = arr[r];
+                        arr[r] = arr[o];
+                        arr[o] = temp;
+                    }
+                }
+            }
+            foreach (int x in arr)
+            {
+                Console.WriteLine(arr[x]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
